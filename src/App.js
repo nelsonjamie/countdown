ee675609d8ee141b5c32edb6654626500f20ed2b
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-11-05 10:51:59 -0700

    Updated code format

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
+import React from 'react';
+import './App.css';
+import Card from './Card.js'
+
+
+class App extends React.Component {
+
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App-header">
+        <Card />
+      </div>
+  </div>)
+ }
 }
 
-export default App;
+export default App
